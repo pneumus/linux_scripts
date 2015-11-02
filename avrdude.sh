d6c1ef51b4f23d8750e26c5ceea4906cda942c10
sonus89 <gal.mateo@gmail.com>
2015-11-02 21:42:34 +0100

    Update avrdude.sh

--- avrdude.sh
+++ avrdude.sh
@@ -1,3 +1,15 @@
+# There is no warranty. Nobody can guarantee that this will work.                   #
+# This script downloads the user-specified *.hex file on                            #
+# Atmel AVR Atmega128 with a JTAG programmer                                        #
+# and creates *.elf *.o and *.hex files as output                                   #
+#                                                                                   #
+# Written by Matthew Gaal                                                           #
+# 2015 - Nov - 1st                                                                  #
+#                                                                                   #
+#####################################################################################
+
+# EXAMPLE: "sh avrdude.sh something" downloads something.hex
+
 #!/bin/bash
 file_name=$1
 
