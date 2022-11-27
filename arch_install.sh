c20165c7bd0dc00487096a98170b444cdc317130
Máté Gál <gal.mateo@gmail.com>
2022-11-28 00:45:09 +0100

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -11,27 +11,33 @@ timedatectl set-ntp true
 # Install Base System
 pacstrap /mnt base base-devel linux linux-firmware vi
 # Generate FSTAB
-#genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+deploy_func () {
+  # Install & Configure Boot Loader
+  pacman --noconfirm -S grub
+  grub-install /dev/sda
+  grub-mkconfig -o /boot/grub/grub.cfg
+  # Install & Enable Network Manager
+  pacman --noconfirm -S networkmanager
+  systemctl enable NetworkManager
+  # Set Locale & Set Language & Set Keyboard Layout
+  sed -i 's/#en_US/en_US/g' /etc/locale.gen
+  echo "LANG=EN-US.UTF-8" > /etc/locale.conf
+  echo "KEYMAP=hu" > /etc/vconsole.conf
+  # Set hostname
+  echo "archlinux" > /etc/hostname 
+  # Set timezone
+  ln -sf /usr/share/zoneinfo/Europe/Budapest /etc/localtime
+  # Set Root Password
+  passwd
+}
+
+export -f deploy_func
 # Change Root into installed Linux
-#arch-chroot /mnt /bin/bash
-# Install & Configure Boot Loader
-#pacman --noconfirm -S grub
-#grub-install /dev/sda
-#grub-mkconfig -o /boot/grub/grub.cfg
-# Install & Enable Network Manager
-#pacman --noconfirm -S networkmanager
-#systemctl enable NetworkManager
-# Set Locale & Set Language & Set Keyboard Layout
-#sed -i 's/#en_US/en_US/g' /etc/locale.gen
-#echo "LANG=EN-US.UTF-8" > /etc/locale.conf
-#echo "KEYMAP=hu" > /etc/vconsole.conf
-# Set hostname
-#echo "archlinux" > /etc/hostname 
-# Set timezone
-#ln -sf /usr/share/zoneinfo/Europe/Budapest /etc/localtime
-# Set Root Password
-#passwd
+arch-chroot /mnt /bin/bash -c "deploy_func"
+
 # Exit Chroot & Unmount & Reboot 
-#exit
-#umount -R /mnt
-#reboot
+exit
+umount -R /mnt
+reboot
