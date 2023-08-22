019524a3070b6e4450771f58a4e7a566ee7cffb5
Máté Gál <gal.mateo@gmail.com>
2023-08-23 01:50:17 +0200

    Update alienware_x14.sh

--- arch/_deploy/alienware_x14.sh
+++ arch/_deploy/alienware_x14.sh
@@ -5,11 +5,21 @@
 # Load the Hungarian Keyboard Layout
 loadkeys hu
 
-# Request Wifi SSID and Password
-echo "Wifi SSID:"
-read wifi_ssid
-echo "Wifi Password:"
-read wifi_password
-iwctl --passphrase $wifi_password station wlan0 connect $ssid
-
+while :
+do
+  # Request Wifi SSID and Password
+  echo "Wifi SSID:"
+  read wifi_ssid
+  echo "Wifi Password:"
+  read wifi_password
+  iwctl --passphrase $wifi_password station wlan0 connect $ssid
+  # Check if Internet is working
+  ping -c 1 gnu.org                   
+  rc=$?
+  if [[ $rc -eq 0 ]] ; then                
+    echo "Internet is fine"
+  else
+    echo "You are offline. Try again!"
+  fi
+done
 
